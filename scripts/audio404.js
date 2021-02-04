81e36431bde396dc7054b65fdc103e98b1c5fbda
Shane Drabing <shane.drabing@gmail.com>
2021-02-03 18:33:19 -0700

    updated icon

--- scripts/audio404.js
+++ scripts/audio404.js
@@ -1,10 +1,8 @@
 var source = "../data/Flechte.mp3"
 var audio = document.createElement("audio");
-//
+
 audio.autoplay = true;
-//
+
 audio.load()
-audio.addEventListener("load", function () {
-    audio.play();
-}, true);
+audio.addEventListener("load", audio.play, true);
 audio.src = source;
