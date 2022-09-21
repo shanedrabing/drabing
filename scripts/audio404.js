46e00fec7a989a5407f431802bb2971641381c7a
Shane Drabing <shane.drabing@gmail.com>
2022-09-21 09:29:55 -0600

    hide all old stuff, new fresh page

--- scripts/audio404.js
+++
@@ -1,8 +0,0 @@
-var source = "../data/Flechte.mp3"
-var audio = document.createElement("audio");
-
-audio.autoplay = true;
-
-audio.load()
-audio.addEventListener("load", audio.play, true);
-audio.src = source;
