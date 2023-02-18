c1e93cb00ace493bc4751871c67c76ca3e943f4b
Shane Drabing <shane.drabing@gmail.com>
2023-02-17 19:38:35 -0700

    link updates after click

--- data/js/deepnote.js
+++ data/js/deepnote.js
@@ -1,6 +1,14 @@
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+  
 $(".doorframe")
-    .one("mouseenter", function () {
+    .one("click", function() {
         $("#player")[0].play();
         $(".doorL")[0].setAttribute("id", "opened");
         $(".doorR")[0].setAttribute("id", "opened");
+
+        sleep(1).then(function() {
+            $(".container a")[0].setAttribute("href", "home.html");
+        });
     });
