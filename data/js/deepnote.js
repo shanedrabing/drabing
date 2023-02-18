8e5db16eb2760e8d4af271dc8d953d624e17bf1f
Shane Drabing <shane.drabing@gmail.com>
2023-02-17 20:42:19 -0700

    less stretch

--- data/js/deepnote.js
+++ data/js/deepnote.js
@@ -5,10 +5,10 @@ function sleep(ms) {
 $(".doorframe")
     .one("click", function() {
         $("#player")[0].play();
-        $("body")[0].setAttribute("id", "opened");
-        $(".doorframe")[0].setAttribute("id", "opened");
         $(".doorL")[0].setAttribute("id", "opened");
         $(".doorR")[0].setAttribute("id", "opened");
+        $(".doorframe")[0].setAttribute("id", "opened");
+        $("body")[0].setAttribute("id", "opened");
 
         sleep(1).then(function() {
             $(".container a")[0].setAttribute("href", "home.html");
