e1c76c9201c665a4a8f29b25fc252261a36feef3
Shane Drabing <shane.drabing@gmail.com>
2023-02-17 20:30:25 -0700

    sick animations

--- data/js/deepnote.js
+++ data/js/deepnote.js
@@ -5,6 +5,8 @@ function sleep(ms) {
 $(".doorframe")
     .one("click", function() {
         $("#player")[0].play();
+        $("body")[0].setAttribute("id", "opened");
+        $(".doorframe")[0].setAttribute("id", "opened");
         $(".doorL")[0].setAttribute("id", "opened");
         $(".doorR")[0].setAttribute("id", "opened");
 
