2e8d3f2bc91c48241e5bae835c7a3eb7ee77f740
Shane Drabing <shane.drabing@gmail.com>
2023-03-25 17:12:49 -0600

    image fade

--- data/js/opus_no1.js
+++ data/js/opus_no1.js
@@ -6,6 +6,8 @@ $(".spinner")
     .one("click", function() {
         $("#player")[0].play();
         $(".spinner")[0].setAttribute("id", "opened");
+        $("body")[0].setAttribute("id", "opened");
+
         // sleep(1).then(function() {
         //     $(".container a")[0].setAttribute("href", "home.html");
         // });
