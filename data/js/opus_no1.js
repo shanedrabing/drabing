1d0931ffd043f1cbe6b9cdc63d0a9690de241fbc
Shane Drabing <shane.drabing@gmail.com>
2023-03-25 16:48:16 -0600

    opus no 1

---
+++ data/js/opus_no1.js
@@ -0,0 +1,12 @@
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+  
+$(".spinner")
+    .one("click", function() {
+        $("#player")[0].play();
+        $(".spinner")[0].setAttribute("id", "opened");
+        // sleep(1).then(function() {
+        //     $(".container a")[0].setAttribute("href", "home.html");
+        // });
+    });
