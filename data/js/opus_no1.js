e42d920bd132eeb8783d280136a0f9d2e16e7218
Shane Drabing <shane.drabing@gmail.com>
2023-03-25 21:01:39 -0600

    repeat music

--- data/js/opus_no1.js
+++ data/js/opus_no1.js
@@ -1,9 +1,14 @@
 function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
-  
+
+$("#player")[0].addEventListener('ended', function () {
+    this.currentTime = 0;
+    this.play();
+}, false);
+
 $(".spinner")
-    .one("click", function() {
+    .one("click", function () {
         $("#player")[0].play();
         $(".spinner")[0].setAttribute("id", "opened");
         $("body")[0].setAttribute("id", "opened");
