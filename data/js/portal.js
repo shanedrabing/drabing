16a0c5844e20f00dff5bbf728d312978eaf1b6d5
Shane Drabing <shane.drabing@gmail.com>
2023-06-12 15:56:36 -0600

    new home page

---
+++ data/js/portal.js
@@ -0,0 +1,12 @@
+opened = function() {    
+    $("#player")[0].play();
+    $(".frameL")[0].setAttribute("id", "opened");
+    $(".frameB")[0].setAttribute("id", "opened");
+    $(".frameT")[0].setAttribute("id", "opened");
+    $(".frameR")[0].setAttribute("id", "opened");
+}
+
+$(".frameL").one("click", opened);
+$(".frameB").one("click", opened);
+$(".frameT").one("click", opened);
+$(".frameR").one("click", opened);
